fd657b3170b9c0931e3820230975556f964c51c6
주연우 <141496501+YeonWoojuice@users.noreply.github.com>
2023-12-06 09:58:32 +0900

    Update maincode.py
    add cat selecting

--- maincode.py
+++ maincode.py
@@ -45,7 +45,7 @@ qu_click_button_x,qu_click_button_y=(1000,1000)
 
 # 시작화면 함수
 def start_screen():
-    x=0 #화면 루프 변수 
+     
     
     background_image = pygame.image.load("D:/python응용/pygame/def start_screen()/start_back_ground.png")  # 실제 이미지 파일 경로로 대체해
     background_image = pygame.transform.scale(background_image, (width, height))
@@ -74,25 +74,66 @@ def start_screen():
             if start_button_rect.collidepoint(mouse_x,mouse_y):
                 screen.blit(click_image,(st_click_button_x-725,st_click_button_y-730+100))
         pygame.display.update()
-
+        
 def story_screen():
-    story1_image = pygame.image.load("D:\python응용\pygame\story\story_1.png")  
-    story1_image = pygame.transform.scale(story1_image, (width, height))
-    screen.blit(story1_image, (0, 0))
+    background_path = pygame.image.load("D:/python응용/pygame/story/story_1.png")  # 실제 이미지 파일 경로로 대체해
+    background_image = pygame.transform.scale(background_path, (width, height))
+    screen.blit(background_image, (0, 0))
 
-    next_button_path="D:/python응용/pygame/def start_screen()/button_start.png"
-    next_story_button=pygame.image.load(next_button_path)
-    button_width,button_height=next_story_button.get_size()
-    nx_button_x, nx_button_y=(400,400)
+    next_button_text=font.render("next >",True,(255,255,255))
+    next_button_rect=next_button_text.get_rect(center=(720,530))
+    screen.blit(next_button_text,next_button_rect)
+    skip_button_text=font.render("skip >>",True,(255,255,255))
+    skip_button_rect=skip_button_text.get_rect(center=(720,50))
+    screen.blit(skip_button_text,skip_button_rect)
+    
+    pygame.display.update()
 
-    next_button_rect=draw_button("next",(420,420))
-
-    story2_image = pygame.image.load("D:\python응용\pygame\story\story_1.png")  
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                    # 이벤트 타입이 마우스버튼을 누르기 이벤트인지 확인 
+                if next_button_rect.collidepoint(event.pos):
+                    print("클릭")
+                    return "next1_story"
+                if skip_button_rect.collidepoint(event.pos):
+                    return "game_ready"
+                
+def next1_story():
+    story2_image = pygame.image.load("D:\python응용\pygame\story\story_2.png")  
     story2_image = pygame.transform.scale(story2_image, (width, height))
+    screen.blit(story2_image, (0, 0))
+
+    next_button_text=font.render("next >",True,(255,255,255))
+    next_button_rect=next_button_text.get_rect(center=(720,530))
+    screen.blit(next_button_text,next_button_rect)
+
+    pygame.display.update()
     
-    screen.blit (next_story_button,(nx_button_x,nx_button_y))
-    
-    mouse_x,mouse_y=pygame.mouse.get_pos()
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                # 이벤트 타입이 마우스버튼을 누르기 이벤트인지 확인 
+                if next_button_rect.collidepoint(event.pos):
+                    print("클릭")
+                    return "next2_story"
+
+
+def next2_story():
+    story2_image = pygame.image.load("D:\python응용\pygame\story\story_3.png")  
+    story2_image = pygame.transform.scale(story2_image, (width, height))
+    screen.blit(story2_image, (0, 0))
+
+    next_button_text=font.render("next >",True,(255,255,255))
+    next_button_rect=next_button_text.get_rect(center=(720,530))
+    screen.blit(next_button_text,next_button_rect)
+    pygame.display.update()
 
     while True:
         for event in pygame.event.get():
@@ -102,16 +143,39 @@ def story_screen():
             elif event.type==pygame.MOUSEBUTTONDOWN:
                 # 이벤트 타입이 마우스버튼을 누르기 이벤트인지 확인 
                 if next_button_rect.collidepoint(event.pos):
-                    screen.blit(story2_image, (0, 0))
+                    print("클릭")
+                    return "next3_story"    
 
-                    return "next_story"
+def next3_story():
+    screen.fill(white)
+    
+    story3_image = pygame.image.load("D:/python응용/pygame/story/story_button.png")  
+    story3_image = pygame.transform.scale(story3_image, (width, height))
+    screen.blit(story3_image, (0, -20))
+    start_button_rect=draw_button("        ",(width//2,280))
+    start_button_rect=draw_button("        ",(width//2,300))
+    start_button_rect=draw_button("        ",(width//2,320))
+ 
 
-# 시작 버튼 다음 화면
-# 시작 배경음악 있어야 하나..?
-# 시작 누르고 다음화면으로 넘어가서 음악고르는 화면
 
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                mouse_x,mouse_y=pygame.mouse.get_pos()
+                if start_button_rect.collidepoint(mouse_x,mouse_y):
+                    return "game_ready"
+            mouse_x,mouse_y=pygame.mouse.get_pos()
+            if start_button_rect.collidepoint(mouse_x,mouse_y):
+                screen.blit(click_image,(550,150))
+        pygame.display.update()
+        
+
+    
 def next_start_screen():
-# 게임 선택 배경화면 
+    # 게임 선택 배경화면 
     background_path = pygame.image.load("D:/python응용/pygame/def next_start_screen()/next_back_grond.jpg")  # 실제 이미지 파일 경로로 대체해
     background_image = pygame.transform.scale(background_path, (width, height))
     screen.blit(background_image, (0, 0))
@@ -139,7 +203,31 @@ def next_start_screen():
 
     screen.blit(music_albom_image, (music_albom_x,music_albom_y))
 
+    black_cat_image = pygame.image.load("D:/python응용/pygame/black_cat0 (1).png")
+    black_cat_image = pygame.transform.scale(black_cat_image,(300,300))
+    three_color_cat_image = pygame.image.load("D:/python응용/pygame/three_color_cat0 (1).png")
+    three_color_cat_image = pygame.transform.scale(three_color_cat_image,(300,300))
+    cheese_cat_image = pygame.image.load("D:/python응용/pygame/cheese_cat0.png")
+    cheese_cat_image = pygame.transform.scale(cheese_cat_image,(300,300))
+    screen.blit(three_color_cat_image,(430,30))
     pygame.display.update()
+    cat_x=0
+    catlist =[three_color_cat_image,black_cat_image,cheese_cat_image]
+    draw_button("<고양이를 선택하세요!>",(590,80))
+
+    cat_select_button_path = "D:/python응용/pygame/def next_start_screen()/next_select_button.png"
+    cat_select_button_right = pygame.image.load(cat_select_button_path)
+    game_start_button_width, game_start_button_height = cat_select_button_right.get_size()
+    screen.blit(cat_select_button_right, (730,190))
+
+    cat_select_button_path = "D:/python응용/pygame/def next_start_screen()/next_select_button_left.png"
+    cat_select_button_left = pygame.image.load(cat_select_button_path)
+    game_start_button_width, game_start_button_height = cat_select_button_left.get_size()
+    screen.blit(cat_select_button_left, (415,190))
+
+
+    pygame.display.update()
+
 
     while True:
         for event in pygame.event.get():
@@ -147,9 +235,22 @@ def next_start_screen():
                 pygame.quit()
                 sys.exit()
             elif event.type==pygame.MOUSEBUTTONDOWN:
+                if(mouse_x>=700 and mouse_x <= 700+60 and mouse_y >= 190-30 and mouse_y<= 190+30):
+                    cat_x+=1
+                    screen.blit(catlist[cat_x%3],(430,30))
+                if(mouse_x>=415-30 and mouse_x <= 415+30 and mouse_y >= 190-30 and mouse_y<= 190+30):
+                    cat_x-=1
+                    screen.blit(catlist[cat_x%3],(430,30))
+                
                 if return_button_rect.collidepoint(event.pos):
                     return "start"
-        mouse_x,mouse_y=pygame.mouse.get_pos()
+            mouse_x,mouse_y=pygame.mouse.get_pos()
+            pygame.display.update()
+    
+# 시작 버튼 다음 화면
+# 시작 배경음악 있어야 하나..?
+# 시작 누르고 다음화면으로 넘어가서 음악고르는 화면
+
 
 # 설명버튼 다음화면
 # 설명에 들어가야할 내용
@@ -175,14 +276,13 @@ def next_question_screen():
 # 버튼그림,rect반환시킴
 
 def draw_button(text, position):
-    button_image = font.render(text, True, (0, 0, 0))
+    font_color=(0,0,0)
+    button_image = font.render(text, True, font_color)
     button_rect = button_image.get_rect(center=position)
     screen.blit(button_image, button_rect)
     return button_rect # 꼭 반환시켜야함
 
-current_state = start_screen()
-next_state = next_start_screen() # 시작화면 표시
-story_state = story_screen() 
+current_state ="start"
 
 running = True # True로 초기화
 while running:
@@ -190,15 +290,21 @@ while running:
         if event.type == pygame.QUIT: # quit를 감지>> running을 False로 설정
             running = False
 
-    if current_state == "story":
-        story_screen()
-    if story_state =="next_story":
-            next_start_screen()
-    if current_state=="next_question":
-        next_question_screen()
-
-    if next_state=="start":
-        start_screen()
+    if current_state == "start":
+        current_state = start_screen()
+    elif current_state == "story":
+        current_state = story_screen()
+    elif current_state == "next_question":
+        current_state = next_question_screen()
+    elif current_state == "next1_story":
+        current_state = next1_story()
+    elif current_state == "next2_story":
+        current_state = next2_story()
+    elif current_state == "next3_story":
+        current_state = next3_story()
+    elif current_state == "game_ready":
+        current_state = next_start_screen()
+    
 
     mouse_x,mouse_y=pygame.mouse.get_pos()    
     pygame.display.flip()
